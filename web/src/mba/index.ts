799dc4fc255ada73330bb822835d6d821c4e19cf
white9ash <1175909514@qq.com>
2025-10-02 09:27:33 +0800

    delete unnecessary files

--- web/src/mba/index.ts
+++
@@ -1,8 +0,0 @@
-// Initialize the wasm module and export the functions that are defined in the wasm module.
-
-import init from './wasm';
-
-const wasm = await init();
-wasm.setPanicHook();
-
-export * from './wasm';
